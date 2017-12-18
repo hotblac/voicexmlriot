85d464a4e3d661161a5fa027ad557c7cbaea0bcb
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2017-12-18 14:55:51 +0000

    Draft API

---
+++ src/main/java/org/vxmlriot/driver/VxmlDriver.java
@@ -0,0 +1,43 @@
+package org.vxmlriot.driver;
+
+import java.util.List;
+
+/**
+ * Main interface to VoiceXMLRiot.
+ * This simulates a VXML browser and user interactions. It allows user input
+ * to be simulated and VXML responses to be verified.
+ */
+public interface VxmlDriver {
+
+    /**
+     * Get a VXML document by URL
+     * @param url of the VXML to be loaded.
+     */
+    void get(String url);
+
+    /**
+     * Simulate user entering a string of DTMF digits
+     * @param digits to be entered
+     */
+    void enterDtmf(String digits);
+
+    /**
+     * Smimulate user saying a sequence of utterances
+     * @param utterance spoken by user
+     */
+    void say(String... utterance);
+
+    /**
+     * Text response spoken by VXML document. Typically, a VXML browser would
+     * render this via TTS.
+     * @return list of text responses of current document
+     */
+    List<String> getTextResponse();
+
+    /**
+     * Audio source files played by VXML document.
+     * @return list of audio responses of current document
+     */
+    List<String> getAudioSrc();
+
+}
