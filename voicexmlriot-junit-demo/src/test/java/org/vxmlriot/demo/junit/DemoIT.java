c7bb0baecace42204afa514c23ca2bc1b7ce7d0b
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2018-01-18 21:26:57 +0000

    Added first VoiceXMLRiot test to DemoIT

--- voicexmlriot-junit-demo/src/test/java/org/vxmlriot/demo/junit/DemoIT.java
+++ voicexmlriot-junit-demo/src/test/java/org/vxmlriot/demo/junit/DemoIT.java
@@ -1,26 +1,44 @@
 package org.vxmlriot.demo.junit;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.vxmlriot.driver.VxmlDriver;
+import org.vxmlriot.driver.VxmlDriverFactory;
+import org.vxmlriot.jvoicexml.JVoiceXmlDriverBuilder;
+import org.vxmlriot.url.RelativeUrlUriBuilder;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 /**
- * A demo integration test using a JVoiceXML driver
+ * A demo integration test using a JVoiceXML driver.
+ * This requires the application to be up and running at APP_ROOT.
+ *
+ * In this example project, the application can be started using
+ * mvn tomcat7:run
+ *
+ * To run as an automated unit test, run
+ * mvn verify
+ * which will start the Tomcat server, run the tests and then shutdown.
  */
 public class DemoIT {
 
+    private static final String APP_ROOT = "http://localhost:9090/voicexmlriot-junit-demo/";
+
+    @BeforeClass
+    public static void  configureDriver() {
+        VxmlDriverFactory.driverBuilder = new JVoiceXmlDriverBuilder()
+                .uriBuilder(new RelativeUrlUriBuilder(APP_ROOT));
+    }
+
     @Test
     public void verifyStart() throws Exception {
-
-        // Quick check that app is running
-        URL url = new URL("http://localhost:9090/voicexmlriot-junit-demo/start.vxml");
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
-
-        assertEquals(200, connection.getResponseCode());
+        VxmlDriver driver = VxmlDriverFactory.getDriver();
+        driver.get("start.vxml");
+        List<String> responses = driver.getTextResponse();
+        assertThat(responses, hasSize(2));
+        assertThat(responses, contains("Hello World!", "Goodbye!"));
     }
 }
